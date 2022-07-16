6bd239bb2c303976f148c5f2a9c2e2f9023e9b9f
Kimberly West <kdenton993@gmail.com>
2022-07-16 17:56:27 -0600

    ability to update tag by id

--- develop/routes/api/tag-routes.js
+++ develop/routes/api/tag-routes.js
@@ -46,24 +46,45 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    Tag_name: req.body.Tag_name
+  },
+  {
+    where:{ id: req.params.id}
+  }
+  )
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
-  Product.destroy({
+  Tag.destroy({
     where: {
-      id:req.params.id
+      id: req.params.id
     }
   })
-  .then(dbProductData) {
-    res.status(404).json({message: "No match"})
-    return;
-  }
-res.json(dbProductData);
-})
-.catch(err => {
-  console.log(err);
-  res.status(500).json(err);
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
 });
-});
+
 module.exports = router;
