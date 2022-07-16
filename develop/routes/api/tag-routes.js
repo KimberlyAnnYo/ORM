a87bb8016eb2cec2433383f5f64735452d182b83
Kimberly West <kdenton993@gmail.com>
2022-07-16 17:53:35 -0600

    adding ability to delete products

--- develop/routes/api/tag-routes.js
+++ develop/routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Tag, Product, ProductTag } = require("../../models");
+const { Tag, Product, ProductTag, Category } = require("../../models");
 
 // The `/api/tags` endpoint
 
@@ -7,7 +7,16 @@ router.get("/", (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   Tag.findAll({
+    attributes:[ 
+      'id',
+      'product_name',
+      'price',
+      'stock'
+    ],
     include: [
+      {model: Category,
+      attributes: ['category_name',]},
+
       {
         model: ProductTag,
       },
@@ -41,6 +50,20 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Product.destroy({
+    where: {
+      id:req.params.id
+    }
+  })
+  .then(dbProductData) {
+    res.status(404).json({message: "No match"})
+    return;
+  }
+res.json(dbProductData);
+})
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
 });
-
+});
 module.exports = router;
